c6a13fdc920a45e766bd59b7a79cce158b2d57af
Austin Meyer <frizz@frizzBook-Pro.local>
2017-09-23 12:13:08 -0400

    made account page and transition to it

---
+++ account.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+
+    $('.container').css('visibility','visible').hide().fadeIn(1000);
+
+});
--- login.js
+++ login.js
@@ -5,9 +5,19 @@ $(document).ready(function() {
         console.log("submitting login form")
         event.preventDefault();
 
-      $('form').fadeOut(500);
-      $('.wrapper').addClass('form-success');
+        $('form').fadeOut(500);
+        $('.wrapper').addClass('form-success');
+        var RESPONSE = true;
+        if (RESPONSE == true) {
+            //login success, wait for animation then go to account page
+            setTimeout(function(){
+                $(".container").fadeOut(1000);
+                location.href = "account.html";
+            }, 1000);
+        } else {
+            //login failed
 
+        }
     });
 
 
