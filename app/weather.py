3421f3d47c527eb706a88023d59c980da7d07174
AustinKlisch <aklisch@wisc.edu>
2017-09-23 12:42:00 -0400

    Add logging and fixing syntax

--- app/weather.py
+++ app/weather.py
@@ -4,12 +4,11 @@ import requests
 import json
 import logging as log
 import difflib
-import time
 
 wordstoremove = ['weather']
 
-def weatherOnly(strin):
 
+def weatherOnly(strin):
     words = strin.split()
     for word in words:
         if len(difflib.get_close_matches(word, wordstoremove, 3 , .8)) > 0:
@@ -27,14 +26,20 @@ def getWeather(strin):
     except pygeolib.GeocoderError as e:
         # If over the query limit retry the request
         if e.status == "OVER_QUERY_LIMIT":
+            log.info("OVER_QUERY_LIMIT Error thrown, retrying request")
             getWeather(strin)
             return;
         elif e.status == "ZERO_RESULTS":
+            log.info("Invalid location requested returning error message")
             return "Invalid location. Please enter: weather followed by CITY,STATE"
         else:
+            log.info("Error thrown: " + e.status)
             return "Error: " + e.status
+
+    # Request from DarkSky
     request = requests.get("https://api.darksky.net/forecast/{0}/{1},{2}".format("3d4089e5787a5054f02ef8fa4f348d2c",
                                                                                  latitude, longitude))
+    #Load the json into an object
     data = json.loads(request.text)
 
     # Create final text to return
