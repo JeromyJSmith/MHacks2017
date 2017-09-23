170868b32cdb2d9c338922b32b83cd06c812a2f8
Kevin Quinn <kpquinn2@wisc.edu>
2017-09-23 06:04:43 -0500

    Simple parsing for input words (Based on Austins news.py implementation)

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ import config
 from flask import Flask, render_template, request
 from twilio.rest import Client
 from twilio.twiml.messaging_response import Message, MessagingResponse
+import difflib
 
+DIFF_SENSITIVITY=0.6
 app = Flask(__name__)
 
 def send_text(body, to_num, from_num):
@@ -11,16 +13,56 @@ def send_text(body, to_num, from_num):
 						from_=from_num,
 						body=body)
 
+def get_api_words():
+    return [
+        {'api': 'wikipedia', 'words': ['wiki', 'wikipedia']},
+        {'api': 'news', 'words': ['news']},
+        {'api': 'weather', 'words': ['rain', 'weather', 'sunny', 'snow']},
+    ]
+
+def pretend_wiki():
+    return 'wikipedia function'
+
+def pretend_news():
+    return 'news function'
+
+def run_api_function(api):
+    options = {
+        # 'wikipedia':
+        'wikipedia': pretend_wiki,
+        'news': pretend_news
+        # 'weather':
+    }
+    return options[api]()
+
+def has_word(string, good_words):
+    return len(difflib.get_close_matches(string, good_words, 1,
+                                        DIFF_SENSITIVITY)) > 0
+
 def get_response_text(body):
-    return 'response text'
+    # hierarchy of responses, help at the top
+    words = body.lower().split()
+    api_words = get_api_words()
+    chosen_api = ''
+    for api in api_words:
+        for word in words:
+            if has_word(word, api['words']):
+                chosen_api = api['api']
+
+    if not chosen_api:
+        return 'Nothing chosen'
+
+    ret_string = run_api_function(chosen_api)
+    if ret_string:
+        return ret_string
 
 @app.route('/sms', methods=['POST'])
 def sms_handler():
-    # hierarchy of responses, help at the top
     body = request.form['Body']
     from_num = request.form['From']
 
     text = get_response_text(body)
+
     resp = MessagingResponse()
     resp.message(text)
     return str(resp)
