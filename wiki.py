853d4b8751791cfa114f13777bc37518dc38dc08
Austin Meyer <frizz@frizzBook-Pro.local>
2017-09-23 05:45:30 -0500

    wiki.py ready for testing

--- wiki.py
+++ wiki.py
@@ -1,13 +1,28 @@
 import wikipedia
 import logging as log
 
+#30 characters less than the max of three texts
+MAX_SUMMARY_LENGTH = 450
+
 def search(str):
+    wordsToRemove = ["wiki ", " wiki", "wikipedia ", " wikipedia", "wikipedia"]
+    for word in wordsToRemove:
+        str = str.replace(word, "")
+
+    log.info("wiki.py: searching for term: " + str)
+
     #get the most important item with that name, if one exists
-    options = wikipedia.search(str, results=1)
-    if len(options) == 0
-        log.info("wiki.py: no items were found by that name in wikipedia")
-        return "No wikipedia page matched your search"
+    try:
+        summary = wikipedia.summary(str)
+    except wikipedia.exceptions.DisambiguationError as e:
+        log.info("wiki.py: multiple possibilities for wiki summaries, choosing most common")
+        summary = wikipedia.summary(e.options[0])
+    except wikipedia.exceptions.PageError as e:
+        log.info("wiki.py: no page found for query: " + str)
+        summary = "No wikipedia page was found that matches your search"
 
-    
+    if len(summary) > MAX_SUMMARY_LENGTH:
+        summary = summary[:450]
+        summary = summary[:summary.rfind(".") + 1]
 
-search("Mercury")
+    return summary
