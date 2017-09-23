54d6041215cdd0e3ab34769eab357ccbb9df1baf
AustinKlisch <aklisch@wisc.edu>
2017-09-23 12:33:25 -0400

    Finished base weather

---
+++ app/weather.py
@@ -0,0 +1,50 @@
+from pygeocoder import Geocoder
+import pygeolib
+import requests
+import json
+import logging as log
+import difflib
+import time
+
+wordstoremove = ['weather']
+
+def weatherOnly(strin):
+
+    words = strin.split()
+    for word in words:
+        if len(difflib.get_close_matches(word, wordstoremove, 3 , .8)) > 0:
+            strin = strin.replace(word + " ", "")
+    return strin
+
+
+def getWeather(strin):
+    location = weatherOnly(strin)
+
+    log.info("Searching for location" + location)
+    try:
+        result = Geocoder.geocode(location)
+        (latitude, longitude) = result.coordinates
+    except pygeolib.GeocoderError as e:
+        # If over the query limit retry the request
+        if e.status == "OVER_QUERY_LIMIT":
+            getWeather(strin)
+            return;
+        elif e.status == "ZERO_RESULTS":
+            return "Invalid location. Please enter: weather followed by CITY,STATE"
+        else:
+            return "Error: " + e.status
+    request = requests.get("https://api.darksky.net/forecast/{0}/{1},{2}".format("3d4089e5787a5054f02ef8fa4f348d2c",
+                                                                                 latitude, longitude))
+    data = json.loads(request.text)
+
+    # Create final text to return
+    finalText = "Forcast for the day:\n" + data['hourly']['summary']
+    finalText += "\nCurrent Temperature: {0}".format(int(data['currently']['temperature'])) + u'\u00b0' + "F\n"
+    finalText += "High Temperature: {0}".format(int(data['daily']['data'][0]['temperatureMax'])) + u'\u00b0' + "F\n"
+    finalText += "Low Temperature: {0}".format(int(data['daily']['data'][0]['temperatureMin'])) + u'\u00b0' + "F"
+
+    # If there is an alert add that to the final text
+    if 'alert' in data:
+        finalText += "\n" + data['alert']['title'] + "\n" + data['alert']['description']
+    print(finalText)
+
