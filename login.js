b4ae277f21f64c6c16ee3c346a261f7a93acd2d8
Austin Meyer <frizz@frizzBook-Pro.local>
2017-09-23 19:01:13 -0400

    more animations

--- login.js
+++ login.js
@@ -7,6 +7,31 @@ $(document).ready(function() {
 
         $('form').fadeOut(500);
         $('.wrapper').addClass('form-success');
+
+        //AJAX CALL TO LOGIN, response is RESPONSE
+
+        var RESPONSE = true;
+        if (RESPONSE == true) {
+            //login success, wait for animation then go to account page
+            setTimeout(function(){
+                $(".container").fadeOut(1000);
+                location.href = "account.html";
+            }, 1000);
+        } else {
+            //login failed
+
+        }
+    });
+
+    $("#sign-up-button").click(function(event){
+        console.log("submitting sign up form")
+        event.preventDefault();
+
+        $('form').fadeOut(500);
+        $('.wrapper').addClass('form-success');
+
+        //AJAX CALL TO CREATE ACCOUNT, response is RESPONSE
+
         var RESPONSE = true;
         if (RESPONSE == true) {
             //login success, wait for animation then go to account page
