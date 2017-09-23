47d8e19a2d7a6fc5fb445bb46d38e866989eaff2
AustinKlisch <aklisch@wisc.edu>
2017-09-23 19:06:57 -0400

    Added help functionality

--- app/text.py
+++ app/text.py
@@ -1,10 +1,11 @@
 import difflib
 import logging as log
 from twilio.rest import Client
-#import config
-import weather, wiki, news
+import config
+import weather, wiki, news, help
 
 DIFF_SENSITIVITY=0.7
+
 
 def send_text(body, to_num, from_num):
     log.debug('Sending text')
@@ -13,30 +14,30 @@ def send_text(body, to_num, from_num):
 						from_=from_num,
 						body=body)
 
+
 def get_api_words():
     return [
         {'api': 'wikipedia', 'words': ['wiki', 'wikipedia']},
         {'api': 'news', 'words': ['news', 'headlines', 'worldnews']},
         {'api': 'weather', 'words': ['weather']},
+        {'api': 'help', 'words': ['help']},
     ]
 
-def pretend_wiki():
-    return 'wikipedia function'
-
-def pretend_news():
-    return 'news function'
 
 def run_api_function(api, body):
     options = {
         'wikipedia': wiki.search,
         'news': news.getNews,
-        'weather': weather.getWeather
+        'weather': weather.getWeather,
+        'help': help.sendHelp
     }
     return options[api](body)
+
 
 def has_word(string, good_words):
     return len(difflib.get_close_matches(string, good_words, 1,
                                         DIFF_SENSITIVITY)) > 0
+
 
 def get_response_text(body):
     # hierarchy of responses, help at the top
@@ -60,3 +61,5 @@ def get_response_text(body):
     ret_string = run_api_function(chosen_api, body)
     if ret_string:
         return ret_string
+
+
